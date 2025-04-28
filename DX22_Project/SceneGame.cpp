4ab6e2517d1c0cb73847dcdaf131911dc02f95d6
shnsknkd <kanade96@outlook.jp>
2025-04-28 15:38:22 +0900

    プロジェクト ファイルを追加します。

---
+++ DX22_Project/SceneGame.cpp
@@ -0,0 +1,20 @@
+#include "SceneGame.h"
+#include "Geometory.h"
+#include "Defines.h"
+
+SceneGame::SceneGame()
+{
+}
+
+SceneGame::~SceneGame()
+{
+}
+
+void SceneGame::Update()
+{
+}
+
+void SceneGame::Draw()
+{
+	Geometory::DrawBox();
+}
---
+++ DX22_Project/SceneGame.h
@@ -0,0 +1,16 @@
+#ifndef __SCENE_GAME_H__
+#define __SCENE_GAME_H__
+
+
+class SceneGame
+{
+public:
+	SceneGame();
+	~SceneGame();
+	void Update();
+	void Draw();
+
+private:
+};
+
+#endif // __SCENE_GAME_H__
